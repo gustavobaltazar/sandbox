5a9edd1679983539bc12d8ad67059b7d7245b9f4
Gustavo Baltazar <35069316877>
2022-08-16 11:03:37 -0300

    Change the button style

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ export const Navbar = () => {
                     <Links linkName="Contact" />
                     <Links linkName="About" />
                     <Links linkName="Wallet" />
-                    <button className="hover:text-2xl rounded-full bg-maincolor text-white">Assinar</button>
+                    <button className="w-20 h-10 text-white rounded-full transition-all duration-[500ms] bg-gradient-to-tl from-pink-500 via-maincolor to-maincolor bg-size-200 bg-pos-0 hover:bg-pos-100">Assinar</button>
                 </div>
             </div>
         </>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,9 +6,20 @@ module.exports = {
 ],
   theme: {
     extend: {
+      transitionDuration: {
+        '0': '0ms',
+        '2000': '2000ms',
+      }, 
       colors: {
         darkmode: "#18181B",
         maincolor: "#8257E5"
+      },
+      backgroundSize: {
+        'size-200': '200% 200%',
+      },
+      backgroundPosition: {
+        'pos-0': '0% 0%',
+        'pos-100': '100% 100%',
       },
     },
   },
