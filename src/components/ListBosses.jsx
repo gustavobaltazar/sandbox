8f83605f1a018098c634a7c302c9759535f147bb
gustavobaltazar <gustavobaltazar31@gmail.com>
2022-09-09 16:57:55 -0300

    Started to get info from api

--- src/components/ListBosses.jsx
+++ src/components/ListBosses.jsx
@@ -1,7 +1,19 @@
+import axios from 'axios'
 export const ListBosses = () => {
+
+  const api = axios.create({
+    baseURL: `http://127.0.0.1:8000/list/boss_show/list_bosses/`
+  })
+  
+  function Api(){
+    api.get('/').then(res => {
+      console.log(res.data)
+    })
+  }
   return (
     <>
       <h1 className="text-red-500">bobo</h1>
+
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,5 +5,4 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ListBosses />
-  
 )
