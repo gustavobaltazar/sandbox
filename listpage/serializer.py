1135f1ec7f3772bc70a3d69b346b2cb66977eacc
gustavobaltazar <gustavobaltazar31@gmail.com>
2022-09-21 16:59:18 -0300

    Add a new field to db

--- listpage/serializer.py
+++ listpage/serializer.py
@@ -6,4 +6,4 @@ from listpage.models import Bosses
 class BossSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bosses
-        fields = ['id', 'name', 'location', 'boss_image']
+        fields = ['id', 'name', 'location', 'boss_image', 'loot']
