3ac82a44b095030d8375b61b26e2fd070c08d9a1
gustavobaltazar <gustavobaltazar31@gmail.com>
2022-09-27 11:33:30 -0300

    Add some Models

--- backend/models.py
+++ backend/models.py
@@ -1,25 +1,59 @@
-from socket import AI_PASSIVE
+from random import choices
 from django.db import models
+
 
 class Endereco(models.Model):
     cidade = models.CharField(max_length=100)
     bairro = models.CharField(max_lenght=100)
-    rua = models.CharField(max_length= 100)
-    
+    rua = models.CharField(max_length=100)
+
+
 class Usuario(models.Model):
+    GOLD = 'G'
+    PLATINUM = 'P'
+    NORMAL = 'N'
+
+    CARD_TYPES = [
+        (NORMAL, 'Normal'),
+        (GOLD, 'Gold'),
+        (PLATINUM, 'Platinum'),
+    ]
     cpf = models.CharField(max_length=15)
+    senha = models.CharField(max_length=100)
+    tipo_conta = models.CharField(max_length=1, )
 
 class Cliente(models.Model):
-   nome = models.CharField(max_length=50)
-   idade = models.DecimalField(max_digits=2)
-   cliente_endereco = models.ForeignKey(Endereco, related_name='Endereco', on_delete=models.CASCADE)
+    SEXO_MASCULINO = 'M'
+    SEXO_FEMININO = 'F'
+
+    SEXO_TIPOS = [
+        (SEXO_MASCULINO, 'Male'),
+        (SEXO_FEMININO, 'Female'),
+    ]
+    nome = models.CharField(max_length=50)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    idade = models.DecimalField(max_digits=2)
+    sexo = models.CharField(max_length=1, choices=SEXO_TIPOS)
+    cliente_endereco = models.ForeignKey(Endereco, related_name='Endereco', on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.user_name
 
 class Cartao(models.Model):
-    pass
-
+    cliente = models.ForeignKey(Cliente, on_delete=models.DO_NOTHING)
+    numero_cartao = models.CharField(max_length=16)
+    cvv = models.CharField(max_length=3)
+    validade  = models.DateField() 
+   
+    def __str__(self) -> str:
+        return self.numero_cartao 
 class Conta(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
     carteira = models.DecimalField(max_digits=7, decimal_places=2)
     cartao_conta = models.ForeignKey(Cartao, related_name='Cartao', on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return self.cliente
 
 class Fatura(models.Model):
     pass
