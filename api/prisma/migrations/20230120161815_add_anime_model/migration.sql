454e4dd38cd6ba6a2a63eed468b329a513aa0c37
gustavobaltazar <gustavobaltazar31@gmail.com>
2023-01-20 13:41:31 -0300

    feat: add anime data persistence through prisma w/ sqlite
    - setup prisma client for ts

---
+++ api/prisma/migrations/20230120161815_add_anime_model/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Anime" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "year" TEXT NOT NULL,
+    "season" TEXT NOT NULL,
+    "genre" TEXT NOT NULL,
+    "score" REAL NOT NULL
+);
--- api/src/index.ts
+++ api/src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import { z } from "zod";
 import crypto from "crypto";
+import { prisma } from "./utils/prisma";
+
 const app = express();
 
 app.use(cors());
@@ -15,25 +17,22 @@ const animeScheme = z.object({
   score: z.number(),
 });
 
-
-type Anime = z.infer<typeof animeScheme> & { id: string };
-
-let animes: Anime[] = [];
-
 app.get("/", (req, res) => {
   return res.status(200).json({ message: "Hello World!" });
 });
 
-app.get("/anime", (req, res) => {
-  return res.status(200).json({ animes });
+app.get("/anime", async (req, res) => {
+  const allAnimes = await prisma.anime.findMany();
+  return res.status(200).json({ animes: allAnimes });
 });
 
-app.post("/anime", (req, res) => {
+app.post("/anime", async (req, res) => {
   try {
     const { name, year, season, genre, score } = animeScheme.parse(req.body);
-    const id = crypto.randomUUID();
-    const newAnime = { id, name, year, season, genre, score };
-    animes.push(newAnime);
+    const newAnime = { name, year, season, genre, score };
+    const animes = await prisma.anime.create({
+      data: newAnime
+    })
     return res.status(200).json({ animes });
   } catch (error) {
     return res.status(400).json({ message: "Bad request error", error });
