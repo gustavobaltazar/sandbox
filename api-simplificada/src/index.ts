9cf4b67445e5043f9b387df7569f12096e5d9e77
gustavobaltazar <gustavobaltazar31@gmail.com>
2023-01-25 10:59:04 -0300

    Demonstração trpc

--- api-simplificada/src/index.ts
+++ api-simplificada/src/index.ts
@@ -1,18 +1,82 @@
 import express from 'express'
 import { z } from 'zod'
 import { prisma } from './utils/prisma';
+import cors from 'cors'
+import * as trpcExpress from '@trpc/server/adapters/express';
+import superjson from 'superjson'
+import { initTRPC, TRPCError } from '@trpc/server';
 
 const app = express();
 
 app.use(express.json())
+app.use(cors({
+    origin: [
+        "http://localhost:5173"
+    ]
+}))
 
-app.get("/", (req, res) => {
-    return res.json({ message: "Deu certo" })
-})
+const t = initTRPC.create({
+    transformer: superjson,
+    errorFormatter({ shape }) {
+      return shape;
+    },
+});
 
 const createAnimeScheme = z.object({
     name: z.string(),
     year: z.string()
+})
+
+const appRouter = t.router({
+    allAnime: t.procedure.query(async () => {
+        try {
+            const allAnimes = await prisma.anime.findMany()
+            return { allAnimes }
+        } catch (error) {
+            throw new TRPCError({
+                code: "INTERNAL_SERVER_ERROR",
+                cause: error,
+                message: "Failed to get all animes"
+            })
+        }
+    }),
+    createAnime: t.procedure
+    .input(createAnimeScheme)
+    .mutation(async ({ input }) => {
+        try {
+            const createAnime = await prisma.anime.create({
+                data: {
+                    name: input.name,
+                    year: input.year
+                }
+            })
+            return {
+                createAnime
+            }
+        } catch (error) {
+            throw new TRPCError({
+                code: "INTERNAL_SERVER_ERROR",
+                cause: error,
+                message: "Failed to create anime"
+            })
+        }
+    }),
+    example: t.procedure.query(() => {
+        return "zimbas"
+    })
+});
+
+export type AppRouter = typeof appRouter;
+
+app.use(
+    '/trpc',
+    trpcExpress.createExpressMiddleware({
+      router: appRouter,
+    }),
+);
+
+app.get("/", (req, res) => {
+    return res.json({ message: "Deu certo" })
 })
 
 app.get("/anime", async (req, res) => {
